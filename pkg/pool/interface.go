f91d1e1e1ed75ae6a690ea3c44d66f3a2065527b
Erik <81354428+X64X2@users.noreply.github.com>
2024-05-01 01:11:46 +0000

    Delete pkg directory

--- pkg/pool/interface.go
+++
@@ -1,9 +0,0 @@
-package pool
-
-//Worker an interface which provides an ability to fetch pages
-type Worker interface {
-	Get(url string)
-}
-
-// Spawn a function which spawn new instance of page fetcher(Worker)
-type Spawn func() Worker
