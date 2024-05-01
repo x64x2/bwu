f6171067b299c08a00ee2ad74b81607866ceac9c
Erik <81354428+X64X2@users.noreply.github.com>
2024-05-01 01:08:44 +0000

    Delete data directory

--- data/gtg.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get("https://www.nairaland.com/magnoliaa")
-soup = BeautifulSoup(response.text, "html.parser")
-
-post = soup.find_all('div',attrs={'data-post-type-id': 1})
-for post in post:
-    print(post.find('a', attrs={'class': 's-link'}).get('href'))
